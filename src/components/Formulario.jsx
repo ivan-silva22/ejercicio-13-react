0ab10841b3d05d07e251bbb55eaea92397bc9f89
Ivan Silva <silvaivan280@gmail.com>
2023-06-04 23:51:25 -0300

    realice la consulta de la API con una funcion consultarAPI

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,33 @@
+import { useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const Formulario = () => {
 
+    // const [clima, setClima] = useState([]);
+
+    useEffect(() =>{
+        consultarAPI();
+    },[]);
+
+    const consultarAPI = async (ubicacion="tucuman",pais="argentina") =>{
+        try {
+            const respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ubicacion},${pais}&appid=1e20b7cdd06e771a348949d89d6ec268&units=metric`);
+            const datos = await respuesta.json();
+            console.log(datos.main);
+        } catch (error) {
+            console.log('error');
+        }
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        let ubicacion = e.target.ubicacion.value;
+        let pais = e.target.pais.value;
+        return consultarAPI(ubicacion, pais);
+    } 
     
     return (
-        <Form className="my-4">
+        <Form className="my-4" onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="ubicacion">
                 <Form.Label>Ubicación:</Form.Label>
                 <Form.Control type="text" placeholder="Tucumán" />
@@ -14,7 +37,7 @@ const Formulario = () => {
                 <Form.Control type="text" placeholder="Argentina" />
             </Form.Group>
             <div className="text-center">
-                <Button variant="primary">Consultar</Button>
+                <Button variant="primary" type="submit">Consultar</Button>
             </div>
         </Form>
 
